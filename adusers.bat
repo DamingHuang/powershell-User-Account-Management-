aac1656d4b73d378a82386a5ef9d91fb41273ede
Commoner <79024012+DamingHuang@users.noreply.github.com>
2025-08-31 22:43:47 -0400

    Update adusers.bat

--- adusers.bat
+++ adusers.bat
@@ -1,4 +1,11 @@
 @echo off
-echo Script started at %date% %time% >> \\WIN-PO0QFHEI7V0\CleanDesktop\Administrator\Desktop\users\log.txt
-powershell.exe -ExecutionPolicy Bypass -File "\\WIN-PO0QFHEI7V0\CleanDesktop\Administrator\Desktop\users\adusers.ps1" >> \\WIN-PO0QFHEI7V0\CleanDesktop\Administrator\Desktop\users\errlog.txt 2>&1
-echo Script finished at  %date% %time% >> \\WIN-PO0QFHEI7V0\CleanDesktop\Administrator\Desktop\users\log.txt
+REM -------------------------------
+REM Batch file to run AD User PowerShell script
+REM -------------------------------
+REM Instructions:
+REM 1. Replace "your-path" with the folder where the script and logs will be stored.
+REM 2. Replace "adusers.ps1" with the name of your PowerShell script.
+
+echo Script started at %date% %time% >> "your-path\log.txt"
+powershell.exe -ExecutionPolicy Bypass -File "your-path\log.txt\adusers.ps1" >> "your-path\errlog.txt"  2>&1
+echo Script finished at  %date% %time% >> "your-path\log.txt"
