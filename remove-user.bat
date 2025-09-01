ea3611e0f44d74391371f7a20edf979344949558
Commoner <79024012+DamingHuang@users.noreply.github.com>
2025-08-31 22:57:28 -0400

    Update remove-user.bat

--- remove-user.bat
+++ remove-user.bat
@@ -1,4 +1,13 @@
 @echo off
-echo Script started at %date% %time% >> \\WIN-PO0QFHEI7V0\CleanDesktop\Administrator\Desktop\users\log.txt
-powershell.exe -ExecutionPolicy Bypass -File "\\WIN-PO0QFHEI7V0\CleanDesktop\Administrator\Desktop\users\remove-user.ps1" >> \\WIN-PO0QFHEI7V0\CleanDesktop\Administrator\Desktop\users\errlog.txt 2>&1
-echo Script finished at  %date% %time% >> \\WIN-PO0QFHEI7V0\CleanDesktop\Administrator\Desktop\users\log.txt
+
+REM -------------------------------
+REM Batch file to run AD User PowerShell script
+REM -------------------------------
+REM Instructions:
+REM 1. Replace "your-path" with the folder where the script and logs will be stored.
+REM 2. Replace "remove-user.ps1" with the name of your PowerShell script.
+
+echo Script started at %date% %time% >> "your-path\log.txt"
+powershell.exe -ExecutionPolicy Bypass -File "your-path\adusers.ps1" >> "your-path\errlog.txt" 2>&1
+
+echo Script finished at  %date% %time% >> "your-path\log.txt"
