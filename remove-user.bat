ce2f3fba0cfcb5604fcf564d41eb01ffe6c56213
Commoner <79024012+DamingHuang@users.noreply.github.com>
2025-08-31 22:50:53 -0400

    Add files via upload

---
+++ remove-user.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Script started at %date% %time% >> \\WIN-PO0QFHEI7V0\CleanDesktop\Administrator\Desktop\users\log.txt
+powershell.exe -ExecutionPolicy Bypass -File "\\WIN-PO0QFHEI7V0\CleanDesktop\Administrator\Desktop\users\remove-user.ps1" >> \\WIN-PO0QFHEI7V0\CleanDesktop\Administrator\Desktop\users\errlog.txt 2>&1
+echo Script finished at  %date% %time% >> \\WIN-PO0QFHEI7V0\CleanDesktop\Administrator\Desktop\users\log.txt
