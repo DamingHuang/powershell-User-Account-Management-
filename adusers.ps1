7e3cab701a3e2d119512e30522134d3d246f6614
Commoner <79024012+DamingHuang@users.noreply.github.com>
2025-08-31 22:39:49 -0400

    Update adusers.ps1

--- adusers.ps1
+++ adusers.ps1
@@ -1,13 +1,13 @@
 $scriptErrors = @()
 
-Import-Csv -Path "\\WIN-PO0QFHEI7V0\CleanDesktop\Administrator\Desktop\users\2.CSV" | ForEach-Object {
+Import-Csv -Path "your-path" | ForEach-Object {
 
     New-ADUser `
         -Name $_.Name `
         -GivenName $_.FirstName `
         -Surname $_.LastName `
         -SamAccountName $_.Username `
-        -UserPrincipalName "$($_.Username)@CNY.com" `
+        -UserPrincipalName "$($_.Username)@domain.com" `
         -Path "CN=Users,DC=CNY,DC=com" `
         -AccountPassword (ConvertTo-SecureString $_.Password -AsPlainText -Force) `
         -Enabled $true `
@@ -27,5 +27,6 @@ if ($scriptErrors.Count -gt 0) {
 } else {
  
     Write-Host "All users from the CSV were processed successfully." -ForegroundColor Green
-    Remove-Item  "\\WIN-PO0QFHEI7V0\CleanDesktop\Administrator\Desktop\users\2.csv"
+    Remove-Item  "your path"
 }
+
